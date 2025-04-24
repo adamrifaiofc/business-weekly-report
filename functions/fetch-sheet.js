0023886b1cb0156265026e01241ec05385724664
adamrifaiofc <adamrifai1996@gmail.com>
2025-04-25 02:35:45 +0700

    Update fetch-sheet.js

--- functions/fetch-sheet.js
+++ functions/fetch-sheet.js
@@ -9,7 +9,7 @@ exports.handler = async (event, context) => {
         const auth = new google.auth.GoogleAuth({
             credentials: {
                 client_email: process.env.GOOGLE_CLIENT_EMAIL,
-                private_key: process.env.GOOGLE_PRIVATE_KEY,
+                private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'),
             },
             scopes: ['https://www.googleapis.com/auth/spreadsheets'],
         });
@@ -23,7 +23,7 @@ exports.handler = async (event, context) => {
             range,
         });
 
-        const htmlString = response.data.values[0][0];
+        const htmlString = response.data.values ? response.data.values[0][0] : null;
         if (!htmlString) {
             return {
                 statusCode: 404,
